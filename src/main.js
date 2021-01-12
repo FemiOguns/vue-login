bfabd2808841ccfd88303cf945ebdf0e738cd1a8
ConceptCodes <conceptcodes@gmail.com>
2021-01-11 19:37:47 -0500

    custom css added, fonts to go next

--- src/main.js
+++ src/main.js
@@ -4,15 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Toast from "vue-toastification"
-//import { VueReCaptcha } from 'vue-recaptcha-v3'
 import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bs-stepper/dist/css/bs-stepper.min.css'
-import "vue-toastification/dist/index.css"
+import '@/assets/main.scss'
 
 createApp(App)
     .use(store)
     .use(router)
     .use(Toast)
-    //.use(VueReCaptcha, { siteKey: process.env.VUE_APP_SITE_KEY })
         .mount('#app')
