cdf0cdd8815f370514973ab83288bf1bacc9160a
ConceptCodes <conceptcodes@gmail.com>
2021-01-13 17:45:10 -0500

    main view is there

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,600;0,800;1,300;1,600;1,800&display=swap');
 
-body {
+* {
     font-family: 'Nunito Sans', sans-serif;
 }
 
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,23 +1,20 @@
 <template>
-    <div id="auth" class="container" style="background-image: url('~@/assets/img/auth-bg.png');">
+    <div id="auth" class="container">
+        <img :src="require('@/assets/img/auth-bg.png')" class="img-backdrop">
         <div class="text-center">
-            <img :src="require('@/assets/img/logo.svg')" class="text-center" >
+            <img :src="require('@/assets/img/logo.svg')" class="text-center logo" >
         </div>
-        <div class="position-absolute bottom-0">
-            <div class="card shadow shadow-lg bg-dark w-100 rounded-top">
-                <div ref="gateway" class="text-center">
-                    <div class="card-body">
-                        <h1 class=" card-title text-white">Cartoons to live for</h1>
-                        <div class="py-3 d-grid gap-2 col-6 mx-auto">
-                            <button class="btn btn-primary">Sign Up</button>
-                            <p class="text-medium caption">Start streaming for $9/month or $90/year</p>
-                            <button class="btn btn-outline-primary">Log in</button>
-                        </div>
-                    </div>
+        <div class="card bg-dark w-100 fixed-bottom rounded-top text-center">
+            <div class="card-body py-3">
+                <h1 class="text-white">Cartoons to live for</h1>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-primary">Sign Up</button>
+                    <p class="text-medium caption">Start streaming for $9/month or $90/year</p>
+                    <button class="btn btn-outline-primary">Log in</button>
                 </div>
-                <login-card ref="login" />
-                <register-card ref="register" />
             </div>
+            <login-card ref="login" />
+            <register-card ref="register" />
         </div>
     </div>
 </template>
@@ -41,5 +38,15 @@ import { defineAsyncComponent, ref, onMounted } from 'vue';
     width: 100%;
     height: 100%;
 }
-
+.logo {
+    
+}
+.img-backdrop {
+    z-index: -1000;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: cover center;
+    overflow: hidden;
+}
 </style>
