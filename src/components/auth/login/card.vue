085679f3493751451030f67f56f06c1c899918eb
ConceptCodes <conceptcodes@gmail.com>
2021-01-15 22:21:36 -0500

    login has nice base style, onto register

--- src/components/auth/login/card.vue
+++ src/components/auth/login/card.vue
@@ -1,36 +1,60 @@
 <template>
     <div id="card" class="card text-center fixed-bottom bg-dark shadow shadow-lg rounded-top">
         <!-- icon here -->
-        <div class="card-body">
-            <div class="form-group">
-                <label class="text-center h1 font-weight-bold text-white">Email</label>
-                <input class="form-control" type="email" placeholder="">
-                <div class="row text-medium text-center">
-                    <a class="d-block" href="">Privacy Policy</a>
-                    <a href="">Subscription Agreement</a>
-                    <a href="">Data Collection Statement</a>
-                </div>
+        <form class="card-body" @submit="login" @reset="resetForm">
+            <div class="form-floating mb-3">
+                <input type="email" v-model="username" class="form-control border border-primary border-bottom" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
             </div>
-        </div>
+            <div class="form-floating">
+                <input type="password" v-model="password" class="form-control border border-primary border-bottom" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <button class="btn btn-primary btn-block mb-3" type="submit">Log in</button>
+        <div class="text-medium d-block position-bottom">
+            <ul class="list-unstyled text-primary">
+                <li>
+                    <a href="">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="">Subscription Agreement</a>
+                </li>
+                <li>
+                    <a href="">Data Collection Statement</a>
+                </li>
+            </ul>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
     import { useToast } from 'vue-toastification'
+    import { useStore } from 'vuex'
     export default {
         name: 'card',
         setup() {
             const toast = useToast();
-            return { toast }
+            const store = useStore()
+            return { toast, store }
         },
         data: ()=> ({
-            cred: {
-                email: null,
-                password: null
-            }
+            username: null,
+            password: null
         }),
         methods: {
-    
+            login(evt) {
+                evt.preventDefault()
+                this.store.dispatch('login',{email: this.username, password}).then((res) => {
+                    if(res) {
+                        this.toast.success('We did it')
+                    }
+                }).catch(err => this.toast.error(err))
+            },
+            resetForm(evt) {
+                this.username = null
+                this.password = null
+            }
         }
     }
 </script>
